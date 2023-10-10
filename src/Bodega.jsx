9fda7ef7529876d8d52d8851cae25d27a4f5106d
yeisson stik gutierrez <stik@yeissons-MacBook-Air.local>
2023-10-10 03:25:29 -0500

    scena settings :construction:

--- src/Bodega.jsx
+++ src/Bodega.jsx
@@ -11,7 +11,7 @@ import CustomMesh from "./CustomMesh";
 
 
 export function Bodega(props) {
-  const { nodes, materials } = useGLTF('./model3D/MDL_Env_Lubricantes.glb')
+  const { nodes, materials } = useGLTF('./model3D/almacen.glb.glb')
 
   const ref2 = useRef();
   const customMaterial = new THREE.MeshStandardMaterial({
@@ -55,4 +55,4 @@ export function Bodega(props) {
   )
 }
 
-useGLTF.preload('./model3D/MDL_Env_Lubricantes.glb')
+useGLTF.preload('./model3D/almacen.glb.glb')
